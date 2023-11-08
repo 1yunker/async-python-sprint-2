cdb2d7575fd3659d74a045a1234f7e1b7a3cb27c
Yuri Pomazkin <1yunker@gmail.com>
2023-11-09 01:31:35 +0500

    Define base classes and methods

--- job.py
+++ job.py
@@ -1,12 +1,46 @@
-class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        pass
+from functools import wraps
+from queue import Queue
+from tracemalloc import start
+from typing import Any, Callable, Generator
 
-    def run(self):
-        pass
+
+class Job:
+    """
+    Задача для планировщика.
+    """
+
+    def __init__(
+            self,
+            target: Callable,
+            args: tuple = None,
+            kwargs: dict = None,
+            start_at="",
+            max_working_time=-1,
+            tries=0,
+            dependencies=[]
+    ):
+        self.__args = args or ()
+        self.__kwargs = kwargs or {}
+        self.__coroutine = target(*self.__args, **self.__kwargs)
+        self.start_at = start_at
+        self.max_working_time = max_working_time
+        self.tries = tries
+        self.dependencies = dependencies
+
+    def run(self) -> None:
+        """
+        Запустить задачу.
+        """
+        self.__coroutine.send(None)
 
     def pause(self):
+        """
+        Поставить задачу на паузу.
+        """
         pass
 
     def stop(self):
+        """
+        Остановить задачу.
+        """
         pass
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,56 @@
+from functools import wraps
+
+
+def coroutine(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        gen = f(*args, **kwargs)
+        gen.send(None)
+        return gen
+    return wrap
+
+
 class Scheduler:
+    """
+    Планировщик задач.
+    Принимает и запускает задачи в соответствии с расписанием.
+    """
+
     def __init__(self, pool_size=10):
         pass
 
     def schedule(self, task):
+        """
+        Добавить задачу в список.
+        """
         pass
 
     def run(self):
+        """
+        Запустить исполнение задач из списка.
+        """
         pass
 
     def restart(self):
+        """
+        Восстановить работу планировщика после штатного завершения.
+        """
         pass
 
     def stop(self):
+        """
+        Остановить работу планировщика.
+        """
+        pass
+
+    def backup(self):
+        """
+        Записать задачи из планировщика в JSON-файл.
+        """
+        pass
+
+    def restore(self):
+        """
+        Восстановить задачи планировщика из JSON-файла.
+        """
         pass
